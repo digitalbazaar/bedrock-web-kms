eaf1cd405546082d66e3efeaa76e05f399f53ef5
Matthew Collier <mcollier@digitalbazaar.com>
2019-04-04 11:05:13 -0400

    Update linting config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,5 @@ module.exports = {
   env: {
     browser: true
   },
-  extends: ['digitalbazaar', 'digitalbazaar/jsdoc'],
-  globals: {
-    crypto: true,
-    localStorage: true,
-    window: true,
-    TextDecoder: true,
-    TextEncoder: true,
-    Uint8Array: true
-  }
+  extends: ['digitalbazaar', 'digitalbazaar/jsdoc']
 }
